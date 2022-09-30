ee0d5641d8321b7dd2ed9752dab6a3613cce41ee
Dustin <vlopezklein@outlook.com>
2022-09-29 19:16:59 -0700

    Added example code
    Added example code to project provided by Gabriel Solomon for CIS59

--- calculator/src/main/java/com/calculator/App.java
+++ calculator/src/main/java/com/calculator/App.java
@@ -1,13 +1,82 @@
 package com.calculator;
+import java.util.*;
 
-/**
- * Hello world!
- *
+/*
+ * Chavvi Calculator Application
+ * Dustin Lopez
+ * CIS-059 SJCC
+ * Menu Functions and User Input Functions based on code provided by Professor Gabriel Solomon
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);                        
+    Character command = '_';
+
+    // loop until user quits
+    while (command != 'q') {
+      printMenu();
+      System.out.print("Enter a command: ");
+      command = menuGetCommand(scan);
+
+      executeCommand(scan, command);
     }
+
+    scan.close();
+  }
+
+  //
+  // menu functions
+  //
+  private static void printMenuLine() {
+    System.out.println(
+      "----------------------------------------------------------"
+    );
+  }
+
+  private static void printMenuCommand(Character command, String desc) {
+    System.out.printf("%s\t%s\n", command, desc);
+  }
+
+  // prints the menu
+  public static void printMenu() {
+    printMenuLine();
+    System.out.println("ChavviCalc");
+    printMenuLine();
+
+    printMenuCommand('q', "Quit");
+
+    printMenuLine();
+  }
+
+  // get first character from input
+  private static Character menuGetCommand(Scanner scan) {
+    Character command = '_';
+
+    String rawInput = scan.nextLine();
+
+    if (rawInput.length() > 0) {
+      rawInput = rawInput.toLowerCase();
+      command = rawInput.charAt(0);
+    }
+
+    return command;
+  }
+
+  // calculator functions
+  private static Boolean executeCommand(Scanner scan, Character command) {
+    Boolean success = true;
+
+    switch (command) {
+      case 'q':
+        System.out.println("Thank you for using Chavvi Calc");
+        break;
+      default:
+        System.out.println("ERROR: Unknown commmand");
+        success = false;
+    }
+
+    return success;
+  }
 }
